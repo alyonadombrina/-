39d072bb4b84544c7608a5dd5031296e24352eca
Алёна <105580854+alyonadombrina@users.noreply.github.com>
2022-05-15 12:03:49 +0300

    Add files via upload

---
+++ ConsoleApplication27.cpp
@@ -0,0 +1,41 @@
+﻿
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+void guess(int a, int b, int x, int kostff) {
+	int k = 0;
+	cout << "Введите числа больше 0:";
+	cin >> a >> b;
+	x = rand() % b + a;
+	cout << "Введите число от " << a << " до " << b << endl;
+	while (1) {
+		k++;
+		cout << "Введите число: ";
+		cin >> kostff;
+		if (kostff != x) {
+			cout << "Вы не угадали:(" << endl;
+			if (kostff < x) cout << "Число больше!" << endl;
+			else if (kostff > x) cout << "Число меньше!" << endl;
+
+		}
+		else {
+			cout << "Вы угадали!!!!!!" << endl;
+			break;
+		}
+
+	}
+	cout << "Спустя " << k << " попыток";
+
+}
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(0));
+	int a=0, b=0,x=0, kostff;
+	guess(a, b, x, kostff);
+	
+    return 0;
+}
+
